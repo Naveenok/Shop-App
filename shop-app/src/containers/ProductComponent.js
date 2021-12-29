7d74a4265960fbca5e666d3a5b88d6c02f4a1c37
Naveen Kumaravelmurugaian <oknaveen9395@gmail.com>
2021-12-29 13:02:10 -0500

    pulled the products data from the fake store api and displayed on screen

--- shop-app/src/containers/ProductComponent.js
+++ shop-app/src/containers/ProductComponent.js
@@ -1,10 +1,30 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
+import { setProducts } from "../redux/actions/productActions";
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
-  const { id, title } = products[0];
+  const rederList = products.map((product) => {
+    const { id, title, image, price, category } = product;
+    return (
+      <div className="four column wide">
+        <div className="ui link cards">
+          <div className="card">
+            <div className="image">
+              <img src={image} alt={title} />
+            </div>
+            <div className="content">
+              <div className="header">{title}</div>
+              <div className="meta price">$ {price}</div>
+              <div className="meta">{category}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  });
+  const dispatch = useDispatch();
 
   const fetchProducts = async () => {
     const response = await axios({
@@ -13,24 +33,14 @@ const ProductComponent = () => {
     }).catch((err) => {
       console.log(err);
     });
-    console.log(response);
+    dispatch(setProducts(response.data));
   };
 
   useEffect(() => {
     fetchProducts();
   }, []);
 
-  return (
-    <div className="four column wide">
-      <div className="ui link cards">
-        <div className="card">
-          <div className="image"></div>
-          <div className="content"></div>
-          <div className="header">{title}</div>
-        </div>
-      </div>
-    </div>
-  );
+  return <>{rederList}</>;
 };
 
 export default ProductComponent;
--- shop-app/src/redux/reducers/productReducer.js
+++ shop-app/src/redux/reducers/productReducer.js
@@ -1,18 +1,12 @@
 import { ActionTypes } from "../constants/action-types";
 
 const initiaState = {
-  products: [
-    {
-      id: 1,
-      title: "Naveen",
-      category: "programmer",
-    },
-  ],
+  products: [],
 };
 export const productReducer = (state = initiaState, { type, payload }) => {
   switch (type) {
     case ActionTypes.SET_PRODUCTS:
-      return { ...initiaState.products, ...payload };
+      return { ...state, products: payload };
     default:
       return state;
   }
